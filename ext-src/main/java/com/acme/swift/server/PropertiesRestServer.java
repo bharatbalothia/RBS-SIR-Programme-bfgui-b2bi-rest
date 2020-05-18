c4b4e7c39f030dbee359e794e671819a4e1de0d8
Your Name <you@example.com>
2020-05-18 12:52:32 +0100

    added /{type} support

--- ext-src/main/java/com/acme/swift/server/PropertiesRestServer.java
+++ ext-src/main/java/com/acme/swift/server/PropertiesRestServer.java
@@ -1,29 +1,42 @@
 package com.acme.swift.server;
 
+import java.io.IOException;
 import java.util.Properties;
 import java.util.logging.Logger;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/properties")
-public class PropertiesRestServer extends BaseRestServer{
+public class PropertiesRestServer extends BaseRestServer {
 
-	
-	
 	private static Logger LOGGER = Logger.getLogger(PropertiesRestServer.class.getName());
 
+	private static Properties gplProperties = null;
+
 	@GET
+	@Path("{type}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Properties getProperties() {
+	public Properties getProperties(@PathParam("type") String type) {
 
-		return props;
+		Properties gplProperties = null;
+		if (type.equalsIgnoreCase("gpl")) {
+
+			if (null == gplProperties) {
+				try {
+					gplProperties = new Properties();
+					gplProperties.load(this.getClass().getResourceAsStream("/gpl.properties"));
+				} catch (IOException ioe) {
+
+					LOGGER.fine("Can't load gpl.properties" + ioe.getMessage());
+				}
+
+			}
+		}
+		return gplProperties;
+
 	}
-	//comment
-
-	
-	
-
 }
