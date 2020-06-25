4e9cbc230c4377a98858b7b4bcbf7ca2dd42ad64
Your Name <you@example.com>
2020-06-25 16:14:46 +0100

    fixed transaction total to sum across pay and pay_arch

--- ext-src/main/java/com/stercomm/customers/rbs/sir/rest/server/FilesRestServer.java
+++ ext-src/main/java/com/stercomm/customers/rbs/sir/rest/server/FilesRestServer.java
@@ -28,6 +28,7 @@ import com.stercomm.customers.rbs.sir.rest.domain.TransactionSearchResult;
 import com.stercomm.customers.rbs.sir.rest.util.FileSearchResultBuilder;
 import com.stercomm.customers.rbs.sir.rest.util.FileSearchWhereClauseBuilder;
 import com.stercomm.customers.rbs.sir.rest.util.TransactionSearchResultBuilder;
+import com.stercomm.customers.rbs.sir.rest.domain.Error;
 import com.sterlingcommerce.woodstock.util.frame.Manager;
 import com.sterlingcommerce.woodstock.util.frame.jdbc.Conn;
 
@@ -82,9 +83,14 @@ public class FilesRestServer extends BaseRestServer {
 
 		// now contstruct the query
 		StringBuffer query = new StringBuffer();
-		query.append(
-				"SELECT bundle_id, filename, reference, btimestamp, btype, entity_id, status, error, wf_id, message_id, "
-						+ "isoutbound, isoverride, service, doc_id, (select count(*) from SCT_PAYMENT p where p.bundle_id = b.bundle_id) as trans FROM SCT_BUNDLE b ");
+		
+		query.append("SELECT bundle_id, filename, reference, btimestamp, btype, entity_id, status, error, wf_id, message_id, "
+					+ "isoutbound, isoverride, service, doc_id, "
+					+ "(select count(*) from SCT_PAYMENT p1, SCT_BUNDLE b1 where p1.bundle_id = b1.bundle_id and "
+					+ "b1.bundle_id=bun.bundle_id)+"
+					+ "(select count(*) from SCT_PAYMENT_ARCHIVE p2, SCT_BUNDLE b2 where p2.bundle_id = b2.bundle_id "
+					+ "and b2.bundle_id=bun.bundle_id) "
+					+ "FROM SCT_BUNDLE bun ");
 
 		// are there any query params, if so create a WHERE?
 		if (uriInfo.getQueryParameters().keySet().size() > 0) {
@@ -142,6 +148,87 @@ public class FilesRestServer extends BaseRestServer {
 	}
 
 	@GET
+	@Path("/{fileid}/transactions/{transactionid}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response doSearchForTransactionByID(@PathParam("fileid") String bundleid,
+			@PathParam("transactionid") String transID, @Context UriInfo uriInfo) {
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		boolean is404=false;
+		
+		TransactionSearchResult result = null;
+
+		// now construct the query
+		StringBuffer query = new StringBuffer();
+		
+		//select p.payment_id, p.payment_bic, b.entity, b.filename, p.reference, p.TRANSACTION_ID, p.type, p.isoutbound, 
+		// p.ptimestamp, b.wf_id, p.SETTLE_DATE, p.SETTLE_AMT, p.status  FROM SCP_PAYMENT p, SCT_BUNDLE b 
+		//where p.payment_id = ?  and p.bundle_id = b.bundle_id
+		
+		query.append("select p.payment_id, p.payment_bic, b.entity, b.filename, p.reference, p.TRANSACTION_ID, p.type, p.isoutbound," + 
+				"		 p.ptimestamp, b.wf_id, p.SETTLE_DATE, p.SETTLE_AMT, p.status  FROM SCP_PAYMENT p, SCT_BUNDLE b " + 
+				"		where p.payment_id = ?  and b.bundle_id = ? and p.bundle_id = b.bundle_id ");
+
+		String fullQuery = query.toString();
+		LOGGER.info("Query : " + fullQuery);
+
+		try {
+			conn = Conn.getConnection();
+			ps = conn.prepareStatement(fullQuery);
+			ps.setLong(1,  Long.parseLong(transID));
+			ps.setLong(2, Long.parseLong(bundleid));
+			rs = ps.executeQuery();
+
+			if (rs.next() == false) {
+				LOGGER.info("ResultSet is empty in Java");
+				is404=true;
+			} else {
+				do {
+					result = toTransactionResult(rs);
+				} while (rs.next());
+			}
+
+		}
+
+		catch (Exception e) {
+			LOGGER.severe("SQL Error searching the SCT PAYMENT table : " + e.getMessage());
+
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (ps != null) {
+					ps.close();
+				}
+				if (conn != null) {
+					Conn.freeConnection(conn);
+				}
+			} catch (SQLException se) {
+				LOGGER.severe("SQL exception : " + se.getMessage());
+			}
+		}
+
+		Status s = (!is404?Status.OK:Status.NOT_FOUND);
+		
+		if (s==Status.OK) {
+			
+			return Response.status(s).entity(result).build();
+		}
+		else {
+			Error e = new Error();
+			e.setAttribute("payment id,bundleid");
+			e.setMessage("Payment id or bundle id not found");
+			
+			return Response.status(s).entity(e).build();
+		}
+		
+
+	}
+
+	@GET
 	@Path("/{fileid}/transactions")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response doSearchForTransactions(@PathParam("fileid") String bundleid, @Context UriInfo uriInfo) {
@@ -170,20 +257,18 @@ public class FilesRestServer extends BaseRestServer {
 
 		// now construct the query
 		StringBuffer query = new StringBuffer();
-		query.append(
-				"SELECT payment_id, transaction_id, settle_date, settle_amt, type,  "+
-						"status, wf_id from (select * from SCT_PAYMENT UNION select * from SCT_PAYMENT_ARCHIVE) "+
-						"WHERE BUNDLE_ID = ? ORDER BY PAYMENT_ID DESC ");
+		query.append("SELECT payment_id, transaction_id, settle_date, settle_amt, type,  "
+				+ "status, wf_id from (select * from SCT_PAYMENT UNION select * from SCT_PAYMENT_ARCHIVE) "
+				+ "WHERE BUNDLE_ID = ? ORDER BY PAYMENT_ID DESC ");
 
 		// append the pagination we worked out earlier
 		query.append(pagination);
 		// where we put results
 		List<TransactionSearchResult> results = new ArrayList<TransactionSearchResult>();
-		
+
 		String fullQuery = query.toString();
 		LOGGER.info("Query : " + fullQuery);
 
-		
 		try {
 			conn = Conn.getConnection();
 			ps = conn.prepareStatement(fullQuery);
@@ -214,8 +299,6 @@ public class FilesRestServer extends BaseRestServer {
 				LOGGER.severe("SQL exception : " + se.getMessage());
 			}
 		}
-
-		
 
 		return Response.status(Status.OK).entity(results).build();
 
@@ -260,7 +343,7 @@ public class FilesRestServer extends BaseRestServer {
 		return result;
 
 	}
-	
+
 	/**
 	 * Create a TransactionSearchResult object from a Row
 	 * 
@@ -270,26 +353,20 @@ public class FilesRestServer extends BaseRestServer {
 	 */
 	private TransactionSearchResult toTransactionResult(ResultSet row) throws SQLException {
 
-
 		int paymentID = row.getInt(1);
 		String transactionID = row.getString(2);
 		long ts = row.getTimestamp(3).getTime();
-		double settleAmt=row.getDouble(4);
+		double settleAmt = row.getDouble(4);
 		String type = row.getString(5);
 		int status = row.getInt(6);
-		int wfid=row.getInt(7);
-	
+		int wfid = row.getInt(7);
+
 		String formattedSettleDate = df.format(new java.util.Date(ts));
 
-		TransactionSearchResult result = new TransactionSearchResultBuilder(paymentID)
-				.withTransactionID(transactionID)
-				.withSettleAmount(settleAmt)
-				.withSettleDate(formattedSettleDate)
-				.withType(type)
-				.withStatus(status)
-				.withWorkflowID(wfid)
-				.build();
-				
+		TransactionSearchResult result = new TransactionSearchResultBuilder(paymentID).withTransactionID(transactionID)
+				.withSettleAmount(settleAmt).withSettleDate(formattedSettleDate).withType(type).withStatus(status)
+				.withWorkflowID(wfid).build();
+
 		return result;
 
 	}
