2b0cdd81e2cd0d52529065723778ff3b714375f7
Your Name <you@example.com>
2020-09-04 15:13:19 +0100

    fixed to create XML process data from req

--- ext-src/main/java/com/stercomm/customers/rbs/sir/rest/server/TransmitRestServer.java
+++ ext-src/main/java/com/stercomm/customers/rbs/sir/rest/server/TransmitRestServer.java
@@ -1,6 +1,9 @@
 package com.stercomm.customers.rbs.sir.rest.server;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
@@ -14,6 +17,14 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import com.stercomm.customers.rbs.sir.rest.domain.TransmitRequest;
 import com.stercomm.customers.rbs.sir.rest.domain.TransmitResponse;
@@ -45,14 +56,14 @@ public class TransmitRestServer extends BaseRestServer {
 	}
 
 	@POST
-	@Path("/")	
+	@Path("/")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response postTransmitRequest(TransmitRequest req) {
 
 		Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
 
 		String bpName = null;
-		TransmitResponse resp=null;
+		TransmitResponse resp = null;
 
 		// what we return if the post failed to validate
 		final Errors list = new Errors();
@@ -65,40 +76,37 @@ public class TransmitRestServer extends BaseRestServer {
 			return Response.status(Status.BAD_REQUEST).entity(errs).build();
 		}
 		// we have a valid transmit object
+		
 		// find the name of the bp to execute
-
 		bpName = getBPName(req);
-		
+
 		InitialWorkFlowContext iwfc = new InitialWorkFlowContext();
-		
 		iwfc.setWorkFlowName(bpName);
-		iwfc.addContentElement("Username", req.getUsername());
-		iwfc.addContentElement(bpName + "/EntityID", req.getEntity());
 		
-		LOGGER.info(iwfc.toString());
-		
-		//in case there is an Error transmitting
-		
-		Error err = null;		
-		
+		//create the process data
+		createDocContent(req, bpName, iwfc);
+	
+		// in case there is an Error transmitting
+		Error err = null;
+
 		try {
-			
+
 			WorkFlowContextCookie cookie = iwfc.start();
 			resp = new TransmitResponse();
 			resp.setWfcID(cookie.getWorkFlowContextId());
 		} catch (InitialWorkFlowContextException e) {
-			
+
 			e.printStackTrace();
-			err=new Error();
+			err = new Error();
 			err.setAttribute("Execution");
 			err.setMessage("Failed to exec BP : " + e.getMessage() + ", check logs");
-		} 
+		}
 
 		if (null == err) {
-			LOGGER.info("Transmitted request : " + req + "to BP : " + bpName +" wfcid=" + resp.getWfcID());
+			LOGGER.info("Transmitted request : " + req + "to BP : " + bpName + " wfcid=" + resp.getWfcID());
 			return Response.status(Status.OK).entity(resp).build();
 		} else {
-			LOGGER.severe("Error transmitting request : " + req + "to BP : " + bpName);
+			LOGGER.severe("Error transmitting request : " + req + " to BP : " + bpName);
 			return Response.status(Status.INTERNAL_SERVER_ERROR).entity(err).build();
 		}
 	}
@@ -136,4 +144,40 @@ public class TransmitRestServer extends BaseRestServer {
 		return retval;
 	}
 
+	private Element parseXmlString(String xml) {
+		try {
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder documentBuilder = dbf.newDocumentBuilder();
+			ByteArrayInputStream bais = new ByteArrayInputStream(xml.getBytes("UTF-8"));
+			org.w3c.dom.Document documentEl = documentBuilder.parse(bais);
+			Element root = documentEl.getDocumentElement();
+			return root;
+		} catch (ParserConfigurationException pce) {
+			throw new RuntimeException("Error setting up XML parser", pce);
+		} catch (UnsupportedEncodingException uee) {
+			throw new RuntimeException("Error setting up XML parser", uee);
+		} catch (IOException ie) {
+			throw new RuntimeException("Error parsing XML.", ie);
+		} catch (SAXException se) {
+			throw new RuntimeException("Error parsing XML.", se);
+		}
+	}
+	
+	private void createDocContent(TransmitRequest req, String bpName, InitialWorkFlowContext context) {
+		
+		StringBuffer message = new StringBuffer("<Root>\n");
+		message.append("<system-account-user-id>" + req.getUsername() + "</system-account-user-id>");
+		message.append("<username>" + req.getUsername() + "</username>");
+		message.append("<" + bpName + "><EntityID>" + req.getEntityID() + "</EntityID></" + bpName + ">");
+		message.append("</Root>\n");
+		Element root = parseXmlString(message.toString());
+		NodeList nodes = root.getChildNodes();
+		for (int i = 0; i < nodes.getLength(); i++) {
+			Node node = nodes.item(i);
+			if (node.getNodeType() == Node.ELEMENT_NODE)
+				context.addContentElement("", node, true);
+		}
+
+		
+	}
 }
